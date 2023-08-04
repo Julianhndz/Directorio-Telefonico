e6d455244ae55394987bfe1e5b92bddbf1e71907
Julianhndz <julian.mhernan@gmail.com>
2023-08-04 18:41:24 -0500

    Creación de sistema SignUp

--- directory/contacts/urls.py
+++ directory/contacts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("signup/", views.signup, name="signup"),
     path("list/", views.contact_list, name="list"),
     path("logout/", views.exit, name="exit"),
     path("create_contact/", views.create_contact, name="create"),
--- directory/contacts/views.py
+++ directory/contacts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Contact
 from .forms import CreateNewContact
 
@@ -31,6 +33,25 @@ def create_contact(request):
     else:
         Contact.objects.create(name=request.POST["name"], number=request.POST["number"])
         return redirect("/list")
+
+
+def signup(request):
+    """
+    Vista creación de usuarios
+    """
+    if request.method == "GET":
+        return render(request, "registration/signup.html", {"form": UserCreationForm})
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user_creation = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+                user_creation.save()
+                redirect("contacts/index.html")
+            except:
+               return render(request, "registration/signup.html", {"form": UserCreationForm,
+                                                            "error": "Usuario ya existe"})
+        return render(request, "registration/signup.html", {"form": UserCreationForm,
+                                                            "error": "Las contraseñas no coinciden."})    
 
 
 def exit(request):
